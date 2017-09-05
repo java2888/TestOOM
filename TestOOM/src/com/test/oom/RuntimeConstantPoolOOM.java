84864036da8ed5a4d1dbe9b97c325d23cd5bdc04
java2888 <2975370834@qq.com>
2017-09-06 03:52:06 +0800

    commit RuntimeconstantPoolOOM.java first.

---
+++ TestOOM/src/com/test/oom/RuntimeConstantPoolOOM.java
@@ -0,0 +1,13 @@
+package com.test.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RuntimeConstantPoolOOM {
+
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>();
+
+	}
+
+}
