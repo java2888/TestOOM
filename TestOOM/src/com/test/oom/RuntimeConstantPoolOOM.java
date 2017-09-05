52b910e36b2693dd4d82084e3f0b33f33525bbc4
java2888 <2975370834@qq.com>
2017-09-06 04:18:56 +0800

    update it.

--- TestOOM/src/com/test/oom/RuntimeConstantPoolOOM.java
+++ TestOOM/src/com/test/oom/RuntimeConstantPoolOOM.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
--XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
+-XX:MetaspaceSize=3m -XX:MaxMetaspaceSize=3m
 */
 public class RuntimeConstantPoolOOM {
 
 	public static void main(String[] args) {
-		List<String> list=new ArrayList<String>();
+		test_intern();
+		 
+		
+/*		List<String> list=new ArrayList<String>();
 		int i=0;
 		while(true){
 			list.add(String.valueOf(i++).intern());
-		}
+			System.out.println("RuntimeConstantPoolOOM. i=" + i);
+		}*/
+	}
+	
+	public static void test_intern(){
+		String str1=new StringBuilder("计算机").append("软件").toString();
+		System.out.println(str1.intern()==str1);
+		
+		String str2=new StringBuilder("ja").append("va").toString();
+		System.out.println(str2.intern()==str2);
 	}
 
 }
