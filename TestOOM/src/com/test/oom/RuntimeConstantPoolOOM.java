138355c3cd5a8def1da46c4a18f88947be25c0d6
java2888 <2975370834@qq.com>
2017-09-06 03:59:43 +0800

    finish RuntimeConstantPoolOOM.java

--- TestOOM/src/com/test/oom/RuntimeConstantPoolOOM.java
+++ TestOOM/src/com/test/oom/RuntimeConstantPoolOOM.java
@@ -3,11 +3,17 @@ package com.test.oom;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+-XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
+*/
 public class RuntimeConstantPoolOOM {
 
 	public static void main(String[] args) {
 		List<String> list=new ArrayList<String>();
-
+		int i=0;
+		while(true){
+			list.add(String.valueOf(i++).intern());
+		}
 	}
 
 }
