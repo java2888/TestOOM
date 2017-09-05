36f8669ebe87f42b31f81582a51e9164d1c137c3
java2888 <2975370834@qq.com>
2017-09-06 01:29:35 +0800

    finished JavaMethodAreaOOM.java in the first time.

--- TestOOM/src/com/test/oom/JavaMethodAreaOOM.java
+++ TestOOM/src/com/test/oom/JavaMethodAreaOOM.java
@@ -1,9 +1,41 @@
 package com.test.oom;
 
-public class JavaMethodAreaOOM {
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-	public static void main(String[] args) {
-		 
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+/*
+-XX:PermSize=10m  -XX:MaxPermSize=10m
+
+-XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m
+
+*/
+public class JavaMethodAreaOOM {
+    private static int i=0;
+	static class OOMObject{
+		
+	}
+	
+	public static void main(String[] args) throws Throwable{
+		
+		while(true){
+			Enhancer enhancer=new Enhancer();
+			enhancer.setSuperclass(OOMObject.class);
+			enhancer.setUseCache(false);
+			enhancer.setCallback(new MethodInterceptor() {				
+				public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+					// TODO Auto-generated method stub
+					return arg3.invokeSuper(arg0, arg2);
+				}
+			});
+			enhancer.create();
+			System.out.println("End. i=" + i++);
+		}
 
 	}
 
